9f7d2f2a411cfcb85fafdf40975e26ec3dff392e
Djúlia Sabrina Preti <djulia_preti@estudante.sesisenai.org.br>
2026-05-13 20:48:13 -0300

    add: adicionar queries customizadas nos repositories de Lamina, Estoque e Expedicao

--- src/main/java/com/smart/appsa/repository/EstoqueRepository.java
+++ src/main/java/com/smart/appsa/repository/EstoqueRepository.java
@@ -1,11 +1,17 @@
 package com.smart.appsa.repository;
 
+import com.smart.appsa.model.Estoque;
+import com.smart.appsa.model.enums.CorEstoque;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.smart.appsa.model.Estoque;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
 
+    List<Estoque> findByCorEstoqueNot(CorEstoque corEstoque);
+   
+    Optional<Estoque> findByPosicaoFisica(int posicaoFisica);
 }
--- src/main/java/com/smart/appsa/repository/ExpedicaoRepository.java
+++ src/main/java/com/smart/appsa/repository/ExpedicaoRepository.java
@@ -1,11 +1,20 @@
 package com.smart.appsa.repository;
 
+import com.smart.appsa.model.Expedicao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.smart.appsa.model.Expedicao;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ExpedicaoRepository extends JpaRepository<Expedicao, Long> {
 
+    boolean existsByOrdemDeProducaoAtual(int ordemDeProducaoAtual);
+
+    Optional<Expedicao> findByOrdemDeProducaoAtual(int ordemDeProducaoAtual);
+
+    @Query("SELECT e.posicaoFisica FROM Expedicao e")
+    List<Integer> findPosicoesOcupadas();
 }
