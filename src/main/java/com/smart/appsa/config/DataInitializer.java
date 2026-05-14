f18dbbd46ffac7ab865c5db08562a98e02f4e006
ArthurJoseBona <arthurjosebona@gmail.com>
2026-05-14 17:47:00 -0300

    add: adicionar `DataInitializer` para popular a tabela Estoque para testes

---
+++ src/main/java/com/smart/appsa/config/DataInitializer.java
@@ -0,0 +1,41 @@
+package com.smart.appsa.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.smart.appsa.model.Estoque;
+import com.smart.appsa.model.enums.CorEstoque;
+import com.smart.appsa.repository.EstoqueRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Autowired
+    private EstoqueRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.deleteAll(); // limpa tudo antes de recriar
+
+        repository.saveAll(List.of(
+            Estoque.builder().posicaoFisica(1).corEstoque(CorEstoque.AZUL).build(),
+            Estoque.builder().posicaoFisica(2).corEstoque(CorEstoque.AZUL).build(),
+            Estoque.builder().posicaoFisica(3).corEstoque(CorEstoque.AZUL).build(),
+            Estoque.builder().posicaoFisica(4).corEstoque(CorEstoque.AZUL).build(),
+            Estoque.builder().posicaoFisica(5).corEstoque(CorEstoque.VERMELHO).build(),
+            Estoque.builder().posicaoFisica(6).corEstoque(CorEstoque.VERMELHO).build(),
+            Estoque.builder().posicaoFisica(7).corEstoque(CorEstoque.PRETO).build(),
+            Estoque.builder().posicaoFisica(8).corEstoque(CorEstoque.VAZIO).build(),
+            Estoque.builder().posicaoFisica(9).corEstoque(CorEstoque.VAZIO).build(),
+            Estoque.builder().posicaoFisica(10).corEstoque(CorEstoque.VAZIO).build(),
+            Estoque.builder().posicaoFisica(11).corEstoque(CorEstoque.VAZIO).build(),
+            Estoque.builder().posicaoFisica(12).corEstoque(CorEstoque.VAZIO).build()
+        ));
+
+        System.out.println(">> Dados iniciais carregados com sucesso!");
+    }
+}
