68facc56b407880ce26f79481b60a0661f05baa1
ArthurJoseBona <arthurjosebona@gmail.com>
2026-05-14 14:41:15 -0300

    add: adicionar rota Post para PedidoController

--- src/main/java/com/smart/appsa/controller/PedidoController.java
+++ src/main/java/com/smart/appsa/controller/PedidoController.java
@@ -2,6 +2,7 @@ package com.smart.appsa.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.smart.appsa.dto.request.PedidoRequestDTO;
 import com.smart.appsa.dto.response.PedidoResponseDTO;
 import com.smart.appsa.service.PedidoService;
 
@@ -14,8 +15,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -25,9 +28,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PedidoController {
     private PedidoService pedidoService;
 
+    @PostMapping("")
+    public ResponseEntity<PedidoResponseDTO> create(@RequestBody PedidoRequestDTO entity) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(pedidoService.create(entity));
+    }
+    
+
     @GetMapping("/{id}")
     public ResponseEntity<PedidoResponseDTO> findById(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(pedidoService.findById(id));
+        return ResponseEntity.ok(pedidoService.findById(id));
     }
     
     @GetMapping("")
