68d7f78f423354169ed870b82017a31318796fa7
arthurjosebona <arthurjosebona@gmail.com>
2026-05-17 15:12:39 -0300

    refactor(dto): remove ipClp from PedidoRequestDTO and delete EstoqueDisponivelDTO
    - PedidoRequestDTO: remove unused ipClp field
    - EstoqueDisponivelDTO: deleted, replaced by EstoqueResponseDTO

--- src/main/java/com/smart/appsa/dto/response/EstoqueDisponivelDTO.java
+++
@@ -1,9 +0,0 @@
-package com.smart.appsa.dto.response;
-
-import java.util.List;
-
-import com.smart.appsa.model.enums.CorEstoque;
-
-public record EstoqueDisponivelDTO(
-    List<CorEstoque> posicoesNaoVazias
-) {} 
