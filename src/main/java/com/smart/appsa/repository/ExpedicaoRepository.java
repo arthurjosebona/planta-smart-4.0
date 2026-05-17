472cc33a52e8fed27a749716c5c018ba98c65600
arthurjosebona <arthurjosebona@gmail.com>
2026-05-17 16:12:40 -0300

    fix(repository): fix column on query to findPosicoesOcupadas

--- src/main/java/com/smart/appsa/repository/ExpedicaoRepository.java
+++ src/main/java/com/smart/appsa/repository/ExpedicaoRepository.java
@@ -1,12 +1,13 @@
 package com.smart.appsa.repository;
 
-import com.smart.appsa.model.Expedicao;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import com.smart.appsa.model.Expedicao;
 
 @Repository
 public interface ExpedicaoRepository extends JpaRepository<Expedicao, Long> {
@@ -15,6 +16,6 @@ public interface ExpedicaoRepository extends JpaRepository<Expedicao, Long> {
     Optional<Expedicao> findByPosicaoFisica(int posicaoFisica);
     Optional<Expedicao> findByOrdemDeProducaoAtual(int ordemDeProducaoAtual);
     Optional<Expedicao> findFirstByOrdemDeProducaoAtualOrderByPosicaoFisicaAsc(Integer ordemDeProducaoAtual);
-    @Query("SELECT e.posicaoFisica FROM Expedicao e WHERE e.vl_op_atual != 0")
+    @Query("SELECT e.posicaoFisica FROM Expedicao e WHERE e.ordemDeProducaoAtual != 0")
     List<Integer> findPosicoesOcupadas();
 }
