d0539dc5a9f44eb7203158eeaf679dd8e71ff332
Djúlia Sabrina Preti <djulia_preti@estudante.sesisenai.org.br>
2026-06-16 21:45:44 -0300

    feat: adiciona ClpIpConfig e ClpConfigController para configuração de IPs dos CLPs

--- backend/planta-smart-4.0/src/main/java/com/smart/appsa/repository/BlocoRepository.java
+++ backend/planta-smart-4.0/src/main/java/com/smart/appsa/repository/BlocoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.smart.appsa.model.Bloco;
+import com.smart.appsa.model.Pedido;
 
 @Repository
 public interface BlocoRepository extends JpaRepository<Bloco, Long> {
