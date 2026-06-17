6115e1ce7b56b34841fb02a95728a0bcd5c0837c
ArthurJoseBona <arthurjosebona@gmail.com>
2026-06-16 21:50:53 -0300

    add: add route to estacoes

--- frontend/planta-smart-4.0/src/router/routes.tsx
+++ frontend/planta-smart-4.0/src/router/routes.tsx
@@ -2,12 +2,14 @@ import DashboardView from '@pages/Dashboard/DashboardView';
 import HomeView from '@pages/Home/HomeView';
 import PedidosView from '@pages/Pedidos/PedidosView';
 import StoreView from '@pages/Store/StoreView';
+import EstacoesView from '@pages/Estacoes/EstacoesView'
 import { Navigate } from 'react-router-dom';
 
 export const routes = [
-  { path: '/', element: <Navigate to="/store/" replace /> },
+  { path: '/', element: <Navigate to="/home/" replace /> },
   { path: '/store', element: <StoreView /> },
   { path: '/dashboard', element: <DashboardView /> },
   { path: '/pedidos', element: <PedidosView /> },
   { path: '/home', element: <HomeView /> },
+  { path: '/estacoes', element: <EstacoesView />},
 ];
